1fa1c3d3b6cec84e832eb62f0b0624023f01f77b
yogendrabaskota <yogendrabaskota18@gmail.com>
2024-07-22 19:22:15 +0545

    successfully created blog using form

--- src/pages/CreateBlog/CreateBlog.jsx
+++ src/pages/CreateBlog/CreateBlog.jsx
@@ -1,18 +1,40 @@
+import { useNavigate } from 'react-router-dom'
 import Navbar from '../../Components/Navbar/Navbar'
 import './CreateBlog.css'
+import axios from 'axios'
 const CreateBlog = () => {
+    const navigate = useNavigate()
+
+    const createBlog = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(e.currentTarget) 
+        const data = Object.fromEntries(formData)
+
+
+        //send above data to api
+        const response = await axios.post("http://localhost:2000/blogs",data)
+        if(response.status == 201){
+            alert(response.data.message)
+            navigate("/")
+        }else{
+            alert("something went wrong")
+        }
+
+
+    }
+
   return (
     <>
     <Navbar />
     <div className="form-container">
-    <form>
+    <form onSubmit={createBlog}>
       <div className="form-group">
         <label htmlFor="title">Title</label>
         <input type="text" id="title" name="title" required />
       </div>
       <div className="form-group">
         <label htmlFor="subtitle">Subtitle</label>
-        <input type="text" id="subtitle" name="subtitle" required />
+        <input type="text" id="subTitle" name="subTitle" required />
       </div>
       <div className="form-group">
         <label htmlFor="description">Description</label>
